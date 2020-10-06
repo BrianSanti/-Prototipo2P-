8a8862b4d0320a518ba4db0ebcedb00081eb60b0
BrianSanti <briansanti80@gmail.com>
2020-10-06 10:10:54 -0600

    Fase uno

---
+++ EXAMEN2/SIC/Conexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototipo1P
+{
+    class Conexion
+    {
+        public OdbcConnection conexion()
+        {
+            //CREACCION DE LA CONEXCION ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=SIC");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //METODO PARA CERRAR LA CONEXION
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
+
+
+
+
+
+    }
+}
